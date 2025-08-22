e3520d79af761a4de65dc2b84e5526761fe39271
samic++ <samichentouf+git2@gmail.com>
2025-08-22 21:45:26 +0000

    fix listener

--- src/listener.rs
+++ src/listener.rs
@@ -1,24 +1,26 @@
-// use std::{io, net::TcpStream};
-// use std::future::Future;
-// use crate::common::{HttpSocket, Stream};
-// use crate::http1::handler::Http1Socket;
+use std::{io};
+use std::future::Future;
+use crate::common::{HttpSocket, Stream};
+use crate::http1::handler::Http1Socket;
 
-// use tokio::net::TcpListener;
-// // use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-// #[allow(unreachable_code)]
-// pub async fn http_listener<F, O, Fut>(address: &str, listener: F)->io::Result<()>
-// where F: Fn(Http1Socket<TcpStream>)->Fut + Send + Clone + Sync + 'static, Fut: Future<Output = O> + Send + 'static
-// {
-//     let server = TcpListener::bind(address).await?;
-//     loop{
-//         let (socket, addr) = server.accept().await?;
-//         let listener=listener.clone();
-//         tokio::spawn(async move{
-//             let hand=Http1Socket::new(socket,addr);
-//             listener(hand).await;
-//         });
-//     }
+#[allow(unreachable_code)]
+pub async fn http_listener<F, S, Fut>(address: &str, listener: F)->io::Result<()>
+where F: Fn(S)->Fut + Send + Clone + Sync + 'static,
+    S: HttpSocket<TcpStream>,
+    Fut: Future<Output = ()> + Send + 'static
+{
+    let server = TcpListener::bind(address).await?;
+    loop{
+        let (socket, addr) = server.accept().await?;
+        let listener=listener.clone();
+        tokio::spawn(async move{
+            let hand=S::new(socket,addr);
+            listener(hand).await;
+        });
+    }
     
-//     Ok(())
-// }
+    Ok(())
+}
